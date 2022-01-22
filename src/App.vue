6f0ac968e03749da324a73d0e83d2a8951869dab
Juan David Zea <vikzea@gmail.com>
2022-01-22 16:32:00 -0500

    refactor: Styles refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-	<div class="h-screen w-screen flex flex-col bg-gray-100">
-		<div class="flex justify-end gap-4 pt-4 pr-4 text-white">
+	<div class="h-screen w-screen relative flex flex-col bg-gray-100 justify-center items-center text-8xl font-bold">
+		<div class="flex absolute top-4 right-4 justify-end gap-4 text-white">
 			<button class="button btn-primary">Login</button>
 			<button class="button btn-primary">Sign In</button>
 		</div>
-		<div class="flex justify-center items-center text-8xl font-bold flex-grow">
+		<div>
 			<span class="text-gray-900">CAPI</span>
 			<span class="text-capi">LOG</span>
+		</div>
+		<div class="flex flex-col text-gray-400 text-2xl text-center">
+			<span>Sistema de logistica interna de envios</span>
+			<span>Flores El Capiro</span>
 		</div>
 	</div>
 </template>

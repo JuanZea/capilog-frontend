64a63fbcb1225b8b25b151c70d8491fa123b7a40
Juan David Zea <vikzea@gmail.com>
2022-01-22 20:45:47 -0500

    refactor: Spelling and style changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-	<div class="h-screen w-screen relative flex flex-col bg-gray-100 justify-center items-center text-8xl font-bold">
+	<div
+		class="h-screen w-screen bg-scrap relative flex flex-col bg-gray-100 justify-center items-center text-8xl font-bold"
+	>
 		<div class="flex absolute top-4 right-4 justify-end gap-4 text-white">
 			<button class="button btn-primary">Iniciar Sesión</button>
 			<button class="button btn-primary">Registrarse</button>
 		</div>
-		<div>
+		<div class="select-none hover:scale-125 duration-100">
 			<span class="text-gray-900">CAPI</span>
 			<span class="text-capi">LOG</span>
 		</div>
 		<div class="flex flex-col text-gray-400 text-2xl text-center">
-			<span>Sistema de logistica interna de envios</span>
+			<span>Sistema de logística interna de envíos</span>
 			<span>Flores El Capiro</span>
 		</div>
 	</div>

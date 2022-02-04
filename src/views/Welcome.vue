942c5cc34f84ffeaa316f5f40f7374700f3e985d
Juan David Zea <vikzea@gmail.com>
2022-02-04 15:57:31 -0500

    refactor: Welcome responsive and unique inputs id

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,13 +1,13 @@
 <template>
-	<div class="relative flex h-full w-full flex-col items-center justify-center text-8xl">
+	<div class="relative flex h-full w-full flex-col items-center justify-center">
 		<div class="absolute top-4 right-4 flex justify-end gap-4 text-white">
 			<router-link class="button btn-primary flex items-center gap-2" to="/login">
 				<UserCircleIcon class="h-6" />
 				Iniciar Sesión
 			</router-link>
 		</div>
-		<CapilogTitle animated />
-		<div class="flex flex-col text-center text-2xl font-bold text-gray-400">
+		<CapilogTitle class="text-6xl lg:text-8xl" animated />
+		<div class="flex flex-col text-center font-bold text-gray-400 lg:text-2xl">
 			<span>Sistema de logística interna de envíos</span>
 			<span>Flores El Capiro</span>
 		</div>
